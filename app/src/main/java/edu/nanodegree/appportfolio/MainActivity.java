31be30743a5692faa70bb765c1698b4f1c2fb55e
root <root@ip-10-47-174-141.ec2.internal>
2015-06-09 02:57:10 +0000

    Committed by Udacity

---
+++ app/src/main/java/edu/nanodegree/appportfolio/MainActivity.java
@@ -0,0 +1,49 @@
+package edu.nanodegree.appportfolio;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clicked(View v) {
+        Context context = getApplicationContext();
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+
+        CharSequence text = String.format("This button will launch %s!", buttonText);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+}
